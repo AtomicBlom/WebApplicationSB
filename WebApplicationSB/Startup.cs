ce209a25cbf926f24e6beccd6544f251a9c02fd1
Steven Blom <atomicblom@binaryvibrance.net>
2016-08-26 10:21:14 +0930

    Add project files.

---
+++ WebApplicationSB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationSB.Startup))]
+namespace WebApplicationSB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
